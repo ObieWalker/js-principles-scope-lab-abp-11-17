68a3f4392d15d63a2738a66f10b4d162348c47c5
Obi <obinnawalker@gmail.com>
2017-11-08 23:12:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName
+const leastFavoriteCustomer = 'mark'
+
+function upperCaseCustomerName(){
+  customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  var bestCustomer= "steve"
+}
+
+function changeLeastFavoriteCustomer(){
+  var leastFavoriteCustomer= "obi"
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer
+  let favoriteCustomer
+}
